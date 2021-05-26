76b1f001dbf9c1b8296877cd121209cd9c801069
Tom <tommiesamwel@gmail.com>
2021-05-26 18:00:31 +0200

    changed UI and added history per table

--- src/User.java
+++ src/User.java
@@ -10,53 +10,55 @@ public class User {
 
     private Database db = new Database();
 
-    public User(){};
+    public User() {
+    }
+    ;
 
-    User(int userId){
+    User(int userId) {
         loadUser(userId);
     }
 
-    void loadUser(int userId){
+    void loadUser(int userId) {
         try {
             Object[] result = db.queryOne("SELECT * FROM `users` WHERE id = ?", String.valueOf(userId));
             id = (Long) result[0];
             name = result[1] != null ? (String) result[1] : null;
-            if (result[2]!= null) light = (int) result[2];
-            if (result[3]!= null) pressure = (int) result[3];
-            if (result[4]!= null) temperature = (int) result[4];
-            if (result[5]!= null) humidity = (int) result[5];
-        } catch (Exception e){
+            if (result[2] != null) light = (int) result[2];
+            if (result[3] != null) pressure = (int) result[3];
+            if (result[4] != null) temperature = (int) result[4];
+            if (result[5] != null) humidity = (int) result[5];
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    void loadUser(String name){
+    void loadUser(String name) {
         try {
             Object[] result = db.queryOne("SELECT * FROM `users` WHERE name = ?", name);
             id = (Long) result[0];
             name = result[1] != null ? (String) result[1] : null;
-            if (result[2]!= null) light = (int) result[2];
-            if (result[3]!= null) pressure = (int) result[3];
-            if (result[4]!= null) temperature = (int) result[4];
-            if (result[5]!= null) humidity = (int) result[5];
-        } catch (Exception e){
+            if (result[2] != null) light = (int) result[2];
+            if (result[3] != null) pressure = (int) result[3];
+            if (result[4] != null) temperature = (int) result[4];
+            if (result[5] != null) humidity = (int) result[5];
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
 
     public void setTemperatureConfig(int temperature) {
-        int result = db.updateOne("UPDATE `users` SET temperature = "+ String.valueOf(temperature)+ " WHERE `id` = " + String.valueOf(id));
+        int result = db.updateOne("UPDATE `users` SET temperature = " + String.valueOf(temperature) + " WHERE `id` = " + String.valueOf(id));
         if (result == 1) this.temperature = temperature;
     }
 
     public void setLightConfig(int light) {
-        int result = db.updateOne("UPDATE `users` SET light = "+ String.valueOf(light)+ " WHERE `id` = " + String.valueOf(id));
+        int result = db.updateOne("UPDATE `users` SET light = " + String.valueOf(light) + " WHERE `id` = " + String.valueOf(id));
         if (result == 1) this.light = light;
     }
 
     public void setPressureConfig(int pressure) {
-        int result = db.updateOne("UPDATE `users` SET pressure = "+ String.valueOf(pressure)+ " WHERE `id` = " + String.valueOf(id));
+        int result = db.updateOne("UPDATE `users` SET pressure = " + String.valueOf(pressure) + " WHERE `id` = " + String.valueOf(id));
         if (result == 1) this.pressure = pressure;
     }
 
